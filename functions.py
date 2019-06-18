384673c2e844790617a554ed3dc9a60cdb75f3c1
DAnashkin <anashkin_32@mail.ru>
2019-06-18 14:38:48 +0300

    игра по угадыванию чисел

---
+++ functions.py
@@ -0,0 +1,91 @@
+import random
+
+
+def read_input_number() -> int:
+    """Метод предлагает пользователю ввести в консоль целое число, обрабатывает ввод и возвращает это число.
+
+    Returns:
+        _input_number:
+    """
+    input_correctly = False
+    input_number = None
+
+    while input_correctly is False:
+        try:
+            input_number = int(input("Введите число: "))
+            input_correctly = True
+
+        except ValueError:
+            print('Введите, пожалуйста число!')
+            input_correctly = False
+
+    return input_number
+
+
+def compare_numbers(input_number: int, hidden_number: int) -> bool:
+    """Метод сравнивает два целых числа и возвращает результат сравнения
+
+    Args:
+        input_number:
+        hidden_number:
+
+    Returns:
+        win: результат сравнения двух переменных
+    """
+    if input_number == hidden_number:
+        print('Победа!')
+        win = True
+
+    else:
+        print('Введенное число больше загаданного') if input_number > hidden_number \
+            else print('Введенное число меньше загаданного')
+        win = False
+
+    return win
+
+
+def read_input_continue_game() -> str:
+    """Метод выводит в консоль предложение продолжить игру и возвращает введенный ответ
+
+    Returns:
+        input_response: введенное пользователем значение.
+    """
+    input_correctly = False
+
+    while input_correctly is False:
+        input_response = str(input('Продолжить? (y/n): '))
+
+        if input_response != 'y' and input_response != 'n':
+            print('Ошибка. Попробуйте ввести один из предложенных вариантов.')
+
+        else:
+            input_correctly = True
+
+            return input_response
+
+
+def game(counter=3, hidden_number=None):
+    """Метод считывает с консоли вводимое число и сравнивает его с hidden_number.
+
+    Args:
+        counter: количество попыток угадать число.
+        hidden_number: загаданное число, с которым будет сравниваться ответ пользователя.
+
+    """
+
+    win = False
+
+    if hidden_number is None:
+        hidden_number = random.randrange(0, 10)
+
+    print('Система сгенерировала случайное число. Количество попыток: {n}'.format(n=str(counter)))
+
+    while win is False and counter != 0:
+        input_number = read_input_number()
+
+        win = compare_numbers(input_number=input_number, hidden_number=hidden_number)
+
+        if win is False:
+            counter -= 1
+            print(f'Осталось попыток: {counter}')
+    print('Загаданное число: ' + str(hidden_number))
---
+++ game.py
@@ -0,0 +1,18 @@
+import random
+
+from functions import game, read_input_continue_game
+
+continue_flag = True                                    # флаги выносим в начало
+
+while continue_flag:
+    hidden_number = random.randrange(0, 100)            # имена переменных должны нести логический смысл
+
+    # game()                                            # это будет просто игрой по-умолчанию
+    # game(counter=5)                                   # это будет игрой с 5-ью попытками вместо трех
+    game(counter=10, hidden_number=hidden_number)       # а это будет игра с числом от 0 до 99 с 10 попытками
+
+    response = read_input_continue_game()
+
+    if response == 'n':
+        continue_flag = False
+        print('Спасибо за игру')
